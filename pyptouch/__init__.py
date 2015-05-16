c9d675867c5d6a6785dc13c076eece32130cdb5c
Terje Elde <terje@elde.net>
2015-05-16 19:14:49 +0000

    docstring

--- pyptouch/__init__.py
+++ pyptouch/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+"""Python driver for P-Touch series of label-printers, with various utilities.
+
+.. moduleauthor:: Terje Elde <terje@elde.net>
+
+"""
 
 __author__ = 'Terje Elde'
 __email__ = 'terje@elde.net'
 __version__ = '0.0.1-devel'
-
