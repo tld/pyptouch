a504bf89b4087f7a14b658f800d005e69b37ec0d
Terje Elde <terje@elde.net>
2015-05-16 15:57:46 +0000

    Initial placeholders.

---
+++ pyptouch/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Terje Elde'
+__email__ = 'terje@elde.net'
+__version__ = '0.0.1-devel'
+
---
+++ pyptouch/test/test_pyptouch.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_pyptouch
+----------------------------------
+
+Tests for `pyptouch` module.
+"""
+
+import unittest
+
+from pyptouch import pyptouch
+
+class TestPyPTouch(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
