066df30bb70cb04ed91eabf4333a1cb78b89bde8
Terje Elde <terje@elde.net>
2019-11-29 12:14:01 +0000

    Change print to logger.

--- pyptouch/reader.py
+++ pyptouch/reader.py
@@ -9,21 +9,13 @@
 
 """
 
-from __future__ import print_function
-
 import io
-from binascii import unhexlify, hexlify
+from binascii import hexlify, unhexlify
+
+from .logger import logger
 
 subs = {}  # Dictionary containing subscriptions to tags.
 cmds = {}  # Dictionary containing subscriptions to commands.
-
-debug = False
-
-
-def dprint(string):
-    if debug:
-        print(string)
-
 
 def sub(byte):
     """Decorator allowing for easy subscription to tags in the stream.
@@ -87,7 +79,6 @@ class PTReader(object):
             input (str or file): Optionally provide a file (path or object)
             that gets read from.
         """
-
         if input:
             self.__init_input(input)
 
@@ -95,90 +86,90 @@ class PTReader(object):
             op = self._input.read(1)
             if not op:  # EOF
                 break
-            dprint("--> op: %r" % op)
+            logger.debug('--> op: %r' % op)
             if op in subs:
-                dprint("  --> found!")
+                logger.debug('  --> found!')
                 subs[op](self)
             else:
-                dprint(" --> op not found: %r" % op)
+                logger.debug(" --> op not found: %r" % op)
 
     @sub('00')  # preamble
     def sub_pre(self):
         """Preamble, can be safely ignored.  It's just a burst of zeros, to
         clear comamnd-buffer on serial-printers."""
-        dprint("--> sub_pre")
+        logger.debug("--> sub_pre")
         self.preamble += 1
 
     @sub('1B')
     def sub_cmd(self):
-        dprint("--> sub_cmd")
+        logger.debug("--> sub_cmd")
         cmd = self._input.read(2)
         if cmd in cmds:
-            dprint("  --> found 2-byte cmd: %r" % hexlify(cmd))
+            logger.debug("  --> found 2-byte cmd: %r" % hexlify(cmd))
             cmds[cmd](self)
         else:
             if cmd[0] in cmds:
                 # one-byte command, but read two, so seek one back.
                 self._input.seek(-1, io.SEEK_CUR)
-                dprint(" --> found 1-byte cmd: %r" % hexlify(cmd[0]))
+                logger.debug(" --> found 1-byte cmd: %r" % hexlify(cmd[0]))
                 cmds[cmd[0]](self)
             else:
-                dprint("  --> UNKNOWN cmd: %r" % hexlify(cmd))
+                logger.debug("  --> UNKNOWN cmd: %r" % hexlify(cmd))
 
     @sub('4D')
     def sub_compression(self):
-        print("--> sub_compression")
+        logger.debug("--> sub_compression")
 
     @sub('67')
     def sub_raster(self):
-        print("--> sub_raster")
+        logger.debug("--> sub_raster")
 
     @sub('1A')
     def sub_print_eject(self):
-        print("--> sub_print_eject")
+        logger.debug("--> sub_print_eject")
 
     @sub('FF')
     def sub_print_no_eject(self):
-        print("--> sub_print_no_eject")
+        logger.debug("--> sub_print_no_eject")
 
     @cmd('40')
     def cmd_initialize(self):
         """Callback handling initialization of printer."""
         if self.preamble:
-            print("--> cmd_initialize (%r bytes of preamble)" % self.preamble)
+            logger.debug("--> cmd_initialize (%r bytes of preamble)" % self.preamble)
         else:
-            print("--> cmd_initialize (no preamble)")
+            logger.debug("--> cmd_initialize (no preamble)")
         self.init += 1
 
     @cmd('6961')
     def cmd_mode(self):
         """Callback handling mode-setting of printer."""
-        print("--> cmd_mode")
+        logger.debug("--> cmd_mode")
 
     @cmd('697A')
     def cmd_set_media_and_quality(self):
         """Callback for setting print-media and -quality."""
-        print("--> cmd_set_media_and_quality")
+        logger.debug("--> cmd_set_media_and_quality")
 
     @cmd('694D')
     def cmd_set_mode(self):
         """Callback handling set mode."""
-        print("--> cmd_set_mode")
+        logger.debug("--> cmd_set_mode")
 
     @cmd('6941')
     def cmd_cut_every(self):
         """Callback handling cut-settings."""
-        print("--> cmd_cut_every")
+        logger.debug("--> cmd_cut_every")
 
     @cmd('694B')
     def cmd_cut_type(self):
         """Callback handling cut-type."""
-        print("--> cmd_cut_type")
+        logger.debug("--> cmd_cut_type")
 
     @cmd('6964')
     def cmd_set_margins(self):
         """Callback handling setting of margins."""
-        print("--> cmd_set_margins")
+        logger.debug("--> cmd_set_margins")
 
 
 if __name__ == '__main__':
@@ -187,7 +178,4 @@ if __name__ == '__main__':
     ptr.read(os.path.join(os.path.dirname(__file__),
                           'test',
                           'python.ptouch'))
-    print()
-    print()
-    print()
-    print("subs: %r" % subs)
+    logger.debug("subs: %r" % subs)
